5d17acd74f38defb09cd1f363daa3d54f106d4cb
Anders Lemke-Holstein <anders@lemke.dk>
2024-03-01 12:37:11 +0100

    Page for feedback

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,6 +22,7 @@ export function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="https://oase.app">App</NavLink>
+                <NavLink href="/feedback">Feedback</NavLink>
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
                 <NavLink href="https://changelog.oase.app">Changelog</NavLink>
